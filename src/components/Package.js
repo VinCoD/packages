29bcb2f7585359139121e3235522bb4255d2b55d
VinCoD <5895.2020@students.ku.ac.ke>
2023-03-15 17:47:16 +0300

    Started styling packages

--- src/components/Package.js
+++ src/components/Package.js
@@ -2,34 +2,38 @@ import React from 'react';
 
 const MyStory = () => {
     return (
-        <div className="h-full w-full">
-            <div className="h-full w-full">
-                <img src="https://www.dropbox.com/s/21nkam76bgxllpu/_DSF5912-min.jpg?dl=1" className="h-10 w-10" alt="package"/>
-            </div>
-            <div>
-                <h2 className="uppercase">Standard</h2>
-                <p>
-                    A3 60 Page Photo-book
-                    <br/>
-                    Two Photographers
-                    <br/>
-                    All Edited Photos
-                    <br/>
-                    Private Online Gallery & Delivery
-                    <br/>
-                    Engagement Photo-shoot
-                    <br/>
-                    Evening Reception Coverage
-                    <br/>
-                    Duration: Till 10pm
-                    <br/>
-                    4-6 Weeks Editing
+        <div className="p-16 w-full h-full">
+            <div className="flex  gap-96 items-center h-3/4 bg-amber-200 w-3/4 p-10">
+                <div className="">
+                    <img src="https://www.dropbox.com/s/21nkam76bgxllpu/_DSF5912-min.jpg?dl=1" className="h-96 w-60" alt="package"/>
+                </div>
+                <div className="">
+                    <h2 className="uppercase">Standard</h2>
+                    <p>
+                        A3 60 Page Photo-book
+                        <br/>
+                        Two Photographers
+                        <br/>
+                        All Edited Photos
+                        <br/>
+                        Private Online Gallery & Delivery
+                        <br/>
+                        Engagement Photo-shoot
+                        <br/>
+                        Evening Reception Coverage
+                        <br/>
+                        Duration: Till 10pm
+                        <br/>
+                        4-6 Weeks Editing
+
+
+                    </p>
                     <p>
                         KES 250,000
                     </p>
-
-                </p>
+                </div>
             </div>
+
 
         </div>
     );
