826ef29d8cb15d7e35a1f4b8d491569844f631eb
VinCoD <5895.2020@students.ku.ac.ke>
2023-03-15 16:55:37 +0300

    Integrated tailwindcss

---
+++ src/components/Package.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const MyStory = () => {
+    return (
+        <div className="h-full w-full">
+            <div className="h-full w-full">
+                <img src="https://www.dropbox.com/s/21nkam76bgxllpu/_DSF5912-min.jpg?dl=1" className="h-10 w-10" alt="package"/>
+            </div>
+            <div>
+                <h2 className="uppercase">Standard</h2>
+                <p>
+                    A3 60 Page Photo-book
+                    <br/>
+                    Two Photographers
+                    <br/>
+                    All Edited Photos
+                    <br/>
+                    Private Online Gallery & Delivery
+                    <br/>
+                    Engagement Photo-shoot
+                    <br/>
+                    Evening Reception Coverage
+                    <br/>
+                    Duration: Till 10pm
+                    <br/>
+                    4-6 Weeks Editing
+                    <p>
+                        KES 250,000
+                    </p>
+
+                </p>
+            </div>
+
+        </div>
+    );
+};
+
+export default MyStory;
