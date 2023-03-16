642274bc54ff59b9b86cbb00f147f44dc66b14b6
VinCoD <5895.2020@students.ku.ac.ke>
2023-03-16 13:58:07 +0300

    Reformated code used Maps, objects and condition for flex-reverse

--- src/App.js
+++ src/App.js
@@ -2,43 +2,53 @@ import logo from './logo.svg';
 import './App.css';
 import Package from "./components/Package";
 
+const packagesList = [
+    {
+        title: "deluxe",
+        src: "https://www.dropbox.com/s/36wj2vzflqw3d20/_DSF9261-min.jpg?dl=1",
+        description: `A3 60 Page Photo-book,Two Photographers All Edited Photos,Private Online Gallery & Delivery,Engagement Photo-shoot,Evening Reception Coverage,Duration: Till 10pm 4-6 Weeks Editing`,
+        price: "KES 250,000"
+    },
+    {
+        title: "STANDARD",
+        src: "https://www.dropbox.com/s/9hd0pa86zx6d73p/DJI_0459-min.jpg?dl=1",
+        flexVal: "-reverse",
+        description: `A3 40 Page Photo-book,Two Photographers All Edited Photos,Private Online Gallery & Delivery,Engagement Photo-shoot,Evening Reception Coverage,Duration: Till 10pm 4-6 Weeks Editing`,
+        price: "KES 200,000"
+    },
+    {
+        title: "BASIC",
+        src: "https://www.dropbox.com/s/9hd0pa86zx6d73p/DJI_0459-min.jpg?dl=1",
+        description: `A3 40 Page Photo-book,Two Photographers All Edited Photos,Private Online Gallery & Delivery,Engagement Photo-shoot,Evening Reception Coverage,Duration: Till 10pm 4-6 Weeks Editing`,
+        price: "KES 150,000"
+    }
+
+]
+
 function App() {
-  return (
-    <div className="App">
-        <div>
-            <h1 className="mt-7 text-4xl md:text-5xl font-light font-brandonGrotesque" >Wedding Packages</h1>
+    return (
+        <div className="App">
+            <div>
+                <h1 className="mt-7 text-4xl md:text-5xl font-light font-brandonGrotesque">Wedding Packages</h1>
 
-            <p className="pt-7 text-lg font-light font-brandonGrotesque px-4 md:px-36">Our packages are designed to cater to all your photography needs,
-                whether it's capturing your wedding day or creating stunning
-                portraits for your family. Each package is customized to
-                fit your unique requirements and budget.</p>
+                <p className="pt-7 text-lg font-light font-brandonGrotesque px-4 md:px-36">Our packages are designed to
+                    cater to all your photography needs,
+                    whether it's capturing your wedding day or creating stunning
+                    portraits for your family. Each package is customized to
+                    fit your unique requirements and budget.</p>
+            </div>
+            {packagesList.map((pkg) => {
+                return <Package
+                    title={pkg.title}
+                    src={pkg.src}
+                    description={pkg.description}
+                    price={pkg.price}
+                    flexVal={pkg.flexVal}
+                />
+            })}
+
         </div>
-      <Package
-          title="deluxe"
-          description1="A3 60 Page Photo-book"
-          description2="Two Photographers"
-          description3="All Edited Photos"
-          description4="Private Online Gallery & Delivery"
-          description5="Engagement Photo-shoot"
-          description6="Evening Reception Coverage"
-          description7="Duration: Till 10pm 4-6 Weeks Editing"
-          price="KES 250,000"
-
-      />
-        <Package
-        title="STANDARD"
-        description1="A3 40 Page Photo-book"
-        description2="Two Photographers"
-        description3="All Edited Photos"
-        description4="Private Online Gallery & Delivery"
-        description5="Engagement Photo-shoot"
-        description6="Evening Reception Coverage"
-        description7="Duration: Till 10pm 4-6 Weeks Editing"
-        price="KES 200,000"
-
-        />
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/components/Package.js
+++ src/components/Package.js
@@ -5,7 +5,10 @@ import React from 'react';
 const MyComponent = (props) => {
     return (
         <div className="w-full mb-10 md:mb-20 px-4 md:px-16 pt-4 md:pt-16">
-            <div className="flex flex-col-reverse md:flex-row bg-grayLightGreen">
+            <div className={`flex flex-col md:flex-row${props.flexVal ? props.flexVal: ''} bg-grayLightGreen`}>
+                <div className="md:w-1/2">
+                    <img className="w-full h-[30vh] md:h-[500px] object-cover" src={props.src} alt="wedding preparation"/>
+                </div>
                 <div className="md:w-1/2">
                     <div className="w-full h-full flex justify-center items-center mb-10 md:mb-0">
                         <div className="flex flex-col items-center">
@@ -13,29 +16,16 @@ const MyComponent = (props) => {
                                 {props.title}
                             </h2>
                             <p className="font-cprime font-light pb-9 md:pb-12">
-                                {props.description1}
-                                <br/>
-                                {props.description2}
-                                <br/>
-                                {props.description3}
-                                <br/>
-                                {props.description4}
-                                <br/>
-                                {props.description5}
-                                <br/>
-                                {props.description6}
-                                <br/>
-                                {props.description7}
-
+                                {props.description.split(",").map((item, index) => {
+                                    return <>{item}<br/></>
+                                })}
 
                                 </p>
                             <p>{props.price}</p>
                         </div>
                     </div>
                 </div>
-                <div className="md:w-1/2">
-                    <img className="w-full h-[30vh] md:h-[500px] object-cover" src="https://www.dropbox.com/s/36wj2vzflqw3d20/_DSF9261-min.jpg?dl=1" alt="wedding preparation"/>
-                </div>
+
             </div>
         </div>
     );
